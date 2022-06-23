d46aa0230f2f146eac246f9297a58903311325f0
Rohit Eswar <tupili.easwar@flipkart.com>
2022-06-23 16:25:21 +0530

    updating the plugin name

--- unsideline/main.go
+++ unsideline/main.go
@@ -18,7 +18,7 @@ func getScanPlugin() interface{} {
 		log.Fatal(err.Error())
 	}
 	s.Launch()
-	p, err := s.GetInterface("em-scan")
+	p, err := s.GetInterface("scan-sideline-em")
 	if err != nil {
 		log.Fatal(err.Error())
 	}
