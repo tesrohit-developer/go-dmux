34d271a9739dfd0fc292dc2962864e5bd19eab6b
Rohit Eswar <tupili.easwar@flipkart.com>
2022-08-09 10:46:48 +0530

    modifying import paths

--- kafka/kafka_source.go
+++ kafka/kafka_source.go
@@ -1,12 +1,10 @@
 package kafka
 
 import (
+	"os"
 	"time"
-    "os"
 
 	"github.com/Shopify/sarama"
-	"github.com/go-dmux/kafka/kazoo-go"
-	"github.com/go-dmux/kafka/consumer-group"
 )
 
 //KafkaSourceHook to track messages coming out of the source in order
@@ -83,11 +81,11 @@ func (k *KafkaSource) Generate(out chan<- interface{}) {
 	}
 
 	if kconf.SASLEnabled {
-    		//sarama config plain by default
-    		config.Net.SASL.User = kconf.SASLUsername
-    		config.Net.SASL.Password = os.Getenv(kconf.SASLPasswordKey)
-    		config.Net.SASL.Enable = true
-    	}
+		//sarama config plain by default
+		config.Net.SASL.User = kconf.SASLUsername
+		config.Net.SASL.Password = os.Getenv(kconf.SASLPasswordKey)
+		config.Net.SASL.Enable = true
+	}
 
 	config.Offsets.ProcessingTimeout = 10 * time.Second
 
