7b9cccd3ee85919d343e8bdd8b7fb5d01869c0ba
Rohit Eswar <tupili.easwar@flipkart.com>
2022-08-11 14:57:36 +0530

    debugging

--- unsideline/main.go
+++ unsideline/main.go
@@ -80,11 +80,11 @@ func healthCheck(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	log.Println("Hi starting the API")
-	scanPlugin = getScanPlugin()
-	unsidelinePlugin = getUnsidelinePlugin()
-	r := mux.NewRouter()
-	r.HandleFunc("/scan/{startRow}/{endRow}", scan)
-	r.HandleFunc("/unsideline/{key}", unsideline)
-	r.HandleFunc("/healthCheck", healthCheck)
+	//scanPlugin = getScanPlugin()
+	//unsidelinePlugin = getUnsidelinePlugin()
+	//r := mux.NewRouter()
+	//r.HandleFunc("/scan/{startRow}/{endRow}", scan)
+	//r.HandleFunc("/unsideline/{key}", unsideline)
+	//r.HandleFunc("/healthCheck", healthCheck)
 	//log.Fatal(http.ListenAndServe(":9951", r))
 }
