b2857177de339d744518d9acd22a915a051c74cd
Rohit Eswar <tupili.easwar@flipkart.com>
2022-12-26 18:35:53 +0530

    changing mod files and making code changes for sideline

---
+++ connection/kafka_http_sideline_conn.go
@@ -0,0 +1,57 @@
+package connection
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/Shopify/sarama"
+	"github.com/flipkart-incubator/go-dmux/core"
+	sink "github.com/flipkart-incubator/go-dmux/http"
+	source "github.com/flipkart-incubator/go-dmux/kafka"
+	"github.com/flipkart-incubator/go-dmux/offset_monitor"
+	"log"
+	"os"
+)
+
+// **************** CONFIG ***********
+
+//KafkaHTTPWithSidelineConn struct to abstract this connections Run
+type KafkaHTTPWithSidelineConn struct {
+	EnableDebugLog bool
+	Conf           interface{}
+	SidelineImpl   interface{}
+}
+
+func (c *KafkaHTTPWithSidelineConn) getConfiguration() *KafkaHTTPConnConfig {
+	data, _ := json.Marshal(c.Conf)
+	var config *KafkaHTTPConnConfig
+	json.Unmarshal(data, &config)
+	return config
+}
+
+//Run method to start this Connection from source to sink
+func (c *KafkaHTTPWithSidelineConn) Run() {
+	conf := c.getConfiguration()
+	fmt.Println("starting go-dmux with conf", conf)
+	if c.EnableDebugLog {
+		// enable sarama logs if booted with debug logs
+		log.Println("enabling sarama logs")
+		sarama.Logger = log.New(os.Stdout, "[Sarama] ", log.LstdFlags)
+	}
+	kafkaMsgFactory := getKafkaHTTPFactory()
+	offMonitor := offset_monitor.GetOffMonitor(conf.OffsetMonitor)
+	src := source.GetKafkaSource(conf.Source, kafkaMsgFactory, offMonitor)
+	offsetTracker := source.GetKafkaOffsetTracker(conf.PendingAcks, src)
+	hook := GetKafkaHook(offsetTracker, c.EnableDebugLog)
+	sk := sink.GetHTTPSink(conf.Dmux.Size, conf.Sink)
+	sk.RegisterHook(hook)
+	src.RegisterHook(hook)
+
+	//hash distribution
+	h := GetKafkaMsgHasher()
+
+	d := core.GetDistribution(conf.Dmux.DistributorType, h)
+
+	dmux := core.GetDmux(conf.Dmux, d)
+	dmux.ConnectWithSideline(src, sk, c.SidelineImpl)
+	dmux.Join()
+}
---
+++ kafka/consumer-group/go.mod
@@ -0,0 +1,3 @@
+module github.com/tesrohit-developer/go-dmux/kafka/consumer-group
+
+go 1.12
